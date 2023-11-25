878cf263458c9c37fdb16f08cfd59397bc860d56
pauai <147432564+pauai@users.noreply.github.com>
2023-11-25 09:48:45 +0000

    Pending changes exported from your codespace

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -10,6 +10,7 @@ import ImageForm from './_components/image-form'
 import CategoryForm from './_components/category-form'
 import PriceForm from './_components/price-form'
 import AttachmentForm from './_components/attachment-form'
+import ChaptersForm from './_components/chapters-form'
 
 type Props = {}
 
@@ -23,9 +24,15 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 
     const course = await db.course.findUnique({
         where: {
-            id: params.courseId
+            id: params.courseId,
+            userId
         },
         include: {
+            chapters: {
+                orderBy: {
+                    position: "asc"
+                }
+            },
             attachments: {
                 orderBy: {
                     createdAt: "desc"
@@ -49,7 +56,8 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
         course.description,
         course.imageUrl,
         course.price,
-        course.categoryId
+        course.categoryId,
+        course.chapters.some(chapter => chapter.isPublished)
     ]
 
     const totalFields = requiredFields.length
@@ -91,9 +99,7 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
                                 Course Chapters
                             </h2>
                         </div>
-                        <div>
-                            TODO: Chapters
-                        </div>
+                        <ChaptersForm initialData={course} courseId={course.id} />
                     </div>
                     <div>
                         <div className='flex items-center gap-x-2'>
